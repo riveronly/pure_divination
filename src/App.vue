2ddba7dfed36830accadd04b5b5c10d7454d91cf
魏源 <weiyuan03@58.com>
2023-07-15 16:09:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ask-god/>
+  </div>
+</template>
+<script>
+import {defineComponent} from "vue";
+import AskGod from "./components/askGod.vue";
+
+export default defineComponent({
+  components: {AskGod}
+})
+
+</script>
+<style>
+</style>
