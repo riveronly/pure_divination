980c252c3666bf4262da28132d9c80a921d0da5f
魏源 <weiyuan03@58.com>
2023-10-24 10:54:34 +0800

    代码优化

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <Loading v-if="isLoading" />
-    <PureDivination v-show="!isLoading" @stopLoading="stopLoading" />
+    <Loading v-if="isLoading"/>
+    <PureDivination v-show="!isLoading" @stopLoading="stopLoading"/>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from "vue";
-  import PureDivination from "./components/PureDivination.vue";
-  import Loading from "./components/Loading.vue";
+import {ref} from "vue";
+import PureDivination from "./components/PureDivination.vue";
+import Loading from "./components/Loading.vue";
 
-  const isLoading = ref(true)
-  const stopLoading = () => {
-    isLoading.value = false
-  }
+const isLoading = ref(true)
+const stopLoading = () => {
+  isLoading.value = false
+}
 </script>
 
 <style>
