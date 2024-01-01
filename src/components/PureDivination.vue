e28ee1cbe87f1d97265adf83f44be90a388b0c19
weiyuan03 <weiyuan03@58.com>
2024-01-01 14:43:14 +0800

    整理代码，优化格式

--- src/components/PureDivination.vue
+++ src/components/PureDivination.vue
@@ -1,38 +1,38 @@
 <template>
-  <!--时间时辰-->
-  <div class="timeNowInfo">
-    <el-text class="bigFont">{{ getLunarDate() }}</el-text>
-    <el-text class="bigFont">{{ getGregorianDate() }}</el-text>
-  </div>
-  <!--卦象结果-->
-  <div v-if="state.refresh" class="resultInfo">
-    <div
-      v-for="(item, index) in [
-        state.resultMonth,
-        state.resultDay,
-        state.resultHour
-      ]"
-      :key="index"
-      class="result"
-    >
-      <div class="bigFont">{{ getTypeNum(index) }} {{ item.type }}</div>
-      <span>{{ item.summary }}</span>
+    <!--时间时辰-->
+    <div class="timeNowInfo">
+        <el-text class="bigFont">{{ getLunarDate() }}</el-text>
+        <el-text class="bigFont">{{ getGregorianDate() }}</el-text>
     </div>
-  </div>
-  <!--更新时间-->
-  <div v-if="state.refresh" class="updateInfo">
-    <div>{{ state.fortuneMsg }}</div>
-    <div>
-      {{
-        state.lunarMonth > state.lunarYearMonth
-          ? state.lunarYearMonth === 0
-            ? ``
-            : `${state.thisYearNumber}年闰${state.lunarYearMonth}月 月份+1`
-          : ``
-      }}
+    <!--卦象结果-->
+    <div v-if="state.refresh" class="resultInfo">
+        <div
+            v-for="(item, index) in [
+                state.resultMonth,
+                state.resultDay,
+                state.resultHour
+            ]"
+            :key="index"
+            class="result"
+        >
+            <div class="bigFont">{{ getTypeNum(index) }} {{ item.type }}</div>
+            <span>{{ item.summary }}</span>
+        </div>
     </div>
-    <div>{{ state.nextUpdateTime }}</div>
-  </div>
+    <!--更新时间-->
+    <div v-if="state.refresh" class="updateInfo">
+        <div>{{ state.fortuneMsg }}</div>
+        <div>
+            {{
+                state.lunarMonth > state.lunarYearMonth
+                    ? state.lunarYearMonth === 0
+                        ? ``
+                        : `${state.thisYearNumber}年闰${state.lunarYearMonth}月 月份+1`
+                    : ``
+            }}
+        </div>
+        <div>{{ state.nextUpdateTime }}</div>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -41,81 +41,81 @@ import { Lunar, LunarYear, Solar, SolarYear } from 'lunar-typescript'
 import { fortuneArray, hexagramArray } from '../config/config.ts'
 
 const state = reactive({
-  resultMonth: { type: '', desc: '', summary: '' },
-  resultDay: { type: '', desc: '', summary: '' },
-  resultHour: { type: '', desc: '', summary: '' },
+    resultMonth: { type: '', desc: '', summary: '' },
+    resultDay: { type: '', desc: '', summary: '' },
+    resultHour: { type: '', desc: '', summary: '' },
 
-  fortuneMsg: '',
+    fortuneMsg: '',
 
-  lunarMonth: 0,
-  lunarDay: 0,
-  lunarHour: 0,
-  refresh: true,
-  nextUpdateTime: '',
-  thisYearNumber: 0,
-  lunarYearMonth: 0
+    lunarMonth: 0,
+    lunarDay: 0,
+    lunarHour: 0,
+    refresh: true,
+    nextUpdateTime: '',
+    thisYearNumber: 0,
+    lunarYearMonth: 0
 })
 
 onMounted(() => {
-  calcResult()
-  evenThough()
+    calcResult()
+    evenThough()
 })
 
 /**
  * 每分钟重新计算 更新剩余分钟数
  */
 const evenThough = () => {
-  millisecondRemaining()
-  setInterval(() => {
     millisecondRemaining()
-  }, 1000)
+    setInterval(() => {
+        millisecondRemaining()
+    }, 1000)
 }
 
 /**
  * 更新剩余分钟数
  */
 const millisecondRemaining = () => {
-  const now = new Date()
-  const nextPeriod = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate(),
-    Math.floor(now.getHours()) + (now.getHours() % 2 === 0 ? 1 : 2),
-    0,
-    0,
-    0
-  ).getTime()
-  const millisecond = nextPeriod - now.getTime()
+    const now = new Date()
+    const nextPeriod = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        Math.floor(now.getHours()) + (now.getHours() % 2 === 0 ? 1 : 2),
+        0,
+        0,
+        0
+    ).getTime()
+    const millisecond = nextPeriod - now.getTime()
 
-  if (millisecond === 0) {
-    state.refresh = false
-    nextTick(() => {
-      calcResult()
-      state.refresh = true
-    })
-  }
+    if (millisecond === 0) {
+        state.refresh = false
+        nextTick(() => {
+            calcResult()
+            state.refresh = true
+        })
+    }
 
-  const minutes = Math.floor(millisecond / 1000 / 60)
-  state.nextUpdateTime = '距下个卦象还剩' + minutes + '+分钟'
-  console.log(state.nextUpdateTime)
+    const minutes = Math.floor(millisecond / 1000 / 60)
+    state.nextUpdateTime = '距下个卦象还剩' + minutes + '+分钟'
+    console.log(state.nextUpdateTime)
 
-  return millisecond
+    return millisecond
 }
 
 /**
  * 获取农历日期
  */
 const getLunarDate = () => {
-  return `${Lunar.fromDate(new Date()).toString()}日${Lunar.fromDate(
-    new Date()
-  ).getTimeZhi()}时`
+    return `${Lunar.fromDate(new Date()).toString()}日${Lunar.fromDate(
+        new Date()
+    ).getTimeZhi()}时`
 }
 
 /**
  * 获取公历日期
  */
 const getGregorianDate = () => {
-  return `${new Date().toLocaleDateString()}日${new Date().getHours()}时`
+    return `${new Date().toLocaleDateString()}日${new Date().getHours()}时`
 }
 
 /**
@@ -123,144 +123,146 @@ const getGregorianDate = () => {
  * @param index
  */
 const getTypeNum = (index: number) => {
-  return index === 0
-    ? state.lunarMonth
-    : index === 1
-      ? state.lunarDay
-      : state.lunarHour
+    return index === 0
+        ? state.lunarMonth
+        : index === 1
+          ? state.lunarDay
+          : state.lunarHour
 }
 
 /**
  * 计算卦象结果
  */
 const calcResult = () => {
-  const solar = Solar.fromDate(new Date())
-  const lunar = solar.getLunar()
+    const solar = Solar.fromDate(new Date())
+    const lunar = solar.getLunar()
 
-  const lunarMonth = lunar.getMonth()
-  const lunarDay = lunar.getDay()
-  const lunarHour = lunar.getTimeZhiIndex() + 1
+    const lunarMonth = lunar.getMonth()
+    const lunarDay = lunar.getDay()
+    const lunarHour = lunar.getTimeZhiIndex() + 1
 
-  let palacePosition: number //宫位(1-6)
-  let positionIndex: number
+    let palacePosition: number //宫位(1-6)
+    let positionIndex: number
 
-  //本年
-  state.thisYearNumber = SolarYear.fromDate(new Date()).getYear()
-  //本年是否有闰月，是闰几月
-  state.lunarYearMonth = LunarYear.fromYear(state.thisYearNumber).getLeapMonth()
+    //本年
+    state.thisYearNumber = SolarYear.fromDate(new Date()).getYear()
+    //本年是否有闰月，是闰几月
+    state.lunarYearMonth = LunarYear.fromYear(
+        state.thisYearNumber
+    ).getLeapMonth()
 
-  //月
-  state.lunarMonth =
-    lunarMonth +
-    (lunarMonth > state.lunarYearMonth
-      ? state.lunarYearMonth === 0
-        ? 0
-        : 1
-      : 0)
-  palacePosition = state.lunarMonth % 6 || 6
-  positionIndex = palacePosition - 1
-  state.resultMonth = hexagramArray[positionIndex]
+    //月
+    state.lunarMonth =
+        lunarMonth +
+        (lunarMonth > state.lunarYearMonth
+            ? state.lunarYearMonth === 0
+                ? 0
+                : 1
+            : 0)
+    palacePosition = state.lunarMonth % 6 || 6
+    positionIndex = palacePosition - 1
+    state.resultMonth = hexagramArray[positionIndex]
 
-  //日
-  state.lunarDay = lunarDay
-  palacePosition += (state.lunarDay - 1) % 6
-  positionIndex = (palacePosition % 6 || 6) - 1
-  state.resultDay = hexagramArray[positionIndex]
-  const fortuneDay = fortuneArray[positionIndex]
+    //日
+    state.lunarDay = lunarDay
+    palacePosition += (state.lunarDay - 1) % 6
+    positionIndex = (palacePosition % 6 || 6) - 1
+    state.resultDay = hexagramArray[positionIndex]
+    const fortuneDay = fortuneArray[positionIndex]
 
-  //时
-  state.lunarHour = lunarHour
-  palacePosition += (state.lunarHour - 1) % 6
-  positionIndex = (palacePosition % 6 || 6) - 1
-  state.resultHour = hexagramArray[positionIndex]
-  state.fortuneMsg = fortuneDay[positionIndex].msg
+    //时
+    state.lunarHour = lunarHour
+    palacePosition += (state.lunarHour - 1) % 6
+    positionIndex = (palacePosition % 6 || 6) - 1
+    state.resultHour = hexagramArray[positionIndex]
+    state.fortuneMsg = fortuneDay[positionIndex].msg
 }
 </script>
 
 <style scoped>
 .timeNowInfo {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 10px;
 }
 
 .bigFont {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 40px;
-  color: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 40px;
+    color: #000;
 }
 
 .resultInfo {
-  display: flex;
-  flex-direction: row;
+    display: flex;
+    flex-direction: row;
 }
 
 .result {
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  align-items: start;
-  text-align: start;
-  padding: 20px;
-  border-radius: 10px;
-  background-color: rgba(255, 255, 255, 0.2);
-  margin-right: 10px;
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: start;
+    text-align: start;
+    padding: 20px;
+    border-radius: 10px;
+    background-color: rgba(255, 255, 255, 0.2);
+    margin-right: 10px;
 }
 
 .result > span {
-  color: #27424c;
+    color: #27424c;
 }
 
 .result:not(:last-child) {
-  opacity: 0;
-  animation: opacity-translate 1s forwards;
+    opacity: 0;
+    animation: opacity-translate 1s forwards;
 }
 
 .result:last-child {
-  opacity: 0;
-  animation: opacity-translate 1s forwards;
+    opacity: 0;
+    animation: opacity-translate 1s forwards;
 }
 
 .updateInfo {
-  color: #27424c;
-  margin-top: 10px;
-  opacity: 0;
-  flex-direction: column;
-  animation: opacity-translate 1s 1s forwards;
+    color: #27424c;
+    margin-top: 10px;
+    opacity: 0;
+    flex-direction: column;
+    animation: opacity-translate 1s 1s forwards;
 }
 
 @keyframes opacity-translate {
-  0% {
-    transform: translateY(-10px);
-    opacity: 0;
-  }
+    0% {
+        transform: translateY(-10px);
+        opacity: 0;
+    }
 
-  100% {
-    transform: translateY(0);
-    opacity: 1;
-  }
+    100% {
+        transform: translateY(0);
+        opacity: 1;
+    }
 }
 
 @media only screen and (max-width: 1080px) {
-  .resultInfo {
-    flex-wrap: wrap;
-  }
+    .resultInfo {
+        flex-wrap: wrap;
+    }
 
-  .result {
-    width: 100%;
-    margin-bottom: 10px;
-  }
+    .result {
+        width: 100%;
+        margin-bottom: 10px;
+    }
 
-  .timeNowInfo {
-    flex-direction: column;
-    align-items: start;
-  }
+    .timeNowInfo {
+        flex-direction: column;
+        align-items: start;
+    }
 
-  .bigFont {
-    font-size: 20px;
-  }
+    .bigFont {
+        font-size: 20px;
+    }
 }
 </style>
