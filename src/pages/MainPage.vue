fff4413644bc3af9cb9146058fd2796d97ff25b0
v-qizhao1 <v-qizhao1@microsoft.com>
2024-04-09 13:55:06 +0800

    重构部分代码

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -6,61 +6,61 @@
         </div>
 
         <div class="resultDiv">
-            <div v-show="ref1Show" ref="ref1" class="result">
-                <div v-for="(item,index) in state.lunarMonthArr" class="resultItem">
-                    {{ index + 1 }} {{ item?.type }}
-                </div>
+            <div ref="ref1" class="result">
+                <div v-for="(item, index) in state.lunarMonthArr" class="resultItem">{{ index + 1 }} {{ item?.type }}</div>
             </div>
-            <div v-show="ref2Show" ref="ref2" class="result">
-                <div v-for="(item,index) in state.lunarDayArr" class="resultItem">
-                    {{ index + 1 + state.lunarMonthArr.length }} {{ item?.type }}
-                </div>
+            <div ref="ref2" class="result">
+                <div v-for="(item, index) in state.lunarDayArr" class="resultItem">{{ index + 1 + state.lunarMonthArr.length }} {{ item?.type }}</div>
             </div>
-            <div v-show="ref3Show" ref="ref3" class="result">
-                <div v-for="(item,index) in state.lunarHourArr" class="resultItem">
+            <div ref="ref3" class="result">
+                <div v-for="(item, index) in state.lunarHourArr" class="resultItem">
                     {{ index + 1 + state.lunarMonthArr.length + state.lunarDayArr.length }} {{ item?.type }}
                 </div>
             </div>
         </div>
-        <div v-if="state.fortuneMsg!==''">{{ state.fortuneMsg }}</div>
+        <div v-if="state.fortuneMsg !== ''">{{ state.fortuneMsg }}</div>
         <div>{{ state.nextUpdateTime }}</div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { nextTick, onMounted, reactive, ref } from 'vue'
-import { Solar } from 'lunar-typescript'
-import { fortuneArray, Hexagram, hexagramArray } from '../config.ts'
+import { nextTick, onMounted, reactive, ref } from "vue";
+import { Solar } from "lunar-typescript";
+import { fortuneArray, Hexagram, hexagramArray } from "../config.ts";
 
-const ref1 = ref<HTMLDivElement | null>(null)
-const ref2 = ref<HTMLDivElement | null>(null)
-const ref3 = ref<HTMLDivElement | null>(null)
+const ref1 = ref<HTMLDivElement | null>(null);
+const ref2 = ref<HTMLDivElement | null>(null);
+const ref3 = ref<HTMLDivElement | null>(null);
 
-const ref1Show = ref<boolean>(true)
-const ref2Show = ref<boolean>(true)
-const ref3Show = ref<boolean>(true)
-
-const state = reactive({
-    lunarMonthArr: Array<Hexagram>(),
-    lunarDayArr: Array<Hexagram>(),
-    lunarHourArr: Array<Hexagram>(),
-    fortuneMsg: '',
+const state = reactive<{
+    lunarMonthArr: Hexagram[];
+    lunarDayArr: Hexagram[];
+    lunarHourArr: Hexagram[];
+    fortuneMsg: string;
+    refresh: boolean;
+    nextUpdateTime: string;
+    nextLunaTime: number;
+}>({
+    lunarMonthArr: [],
+    lunarDayArr: [],
+    lunarHourArr: [],
+    fortuneMsg: "",
     refresh: true,
-    nextUpdateTime: '',
-    nextLunaTime: 0
-})
+    nextUpdateTime: "",
+    nextLunaTime: 0,
+});
 
 onMounted(() => {
-    getDivination()
-    getRefreshTime()
-    updateRemaining()
+    getDivination();
+    getRefreshTime();
+    updateRemaining();
     setInterval(() => {
-        updateRemaining()
-    }, 1000)
-})
+        updateRemaining();
+    }, 1000);
+});
 
 const getRefreshTime = () => {
-    const now = new Date()
+    const now = new Date();
     state.nextLunaTime = new Date(
         now.getFullYear(),
         now.getMonth(),
@@ -69,104 +69,131 @@ const getRefreshTime = () => {
         0,
         0,
         0
-    ).getTime()
-}
+    ).getTime();
+};
 
 /**
  * 更新 剩余时间
  */
 const updateRemaining = () => {
-    const millisecond = state.nextLunaTime - new Date().getTime()
-    const second = Math.floor(millisecond / 1000)
-    state.nextUpdateTime = '剩余' + (Math.floor(second / 60) + 1) + '分钟'
+    const millisecond = state.nextLunaTime - new Date().getTime();
+    const second = Math.floor(millisecond / 1000);
+    state.nextUpdateTime = "剩余" + (Math.floor(second / 60) + 1) + "分钟";
     if (second <= 0) {
-        refreshMain()
+        refreshMain();
     }
-    return millisecond
-}
+    return millisecond;
+};
 
 const refreshMain = () => {
-    state.refresh = false
+    state.refresh = false;
     nextTick(() => {
-        getDivination()
-        getRefreshTime()
-        state.refresh = true
-    })
-}
+        getDivination();
+        getRefreshTime();
+        state.refresh = true;
+    });
+};
 
 /**
  * 获取农历日期
  */
 const getLunarDate = () => {
-    let lunar = Solar.fromDate(new Date()).getLunar()
-    return `${lunar}${lunar.getTimeZhi()}时`
-}
+    let lunar = Solar.fromDate(new Date()).getLunar();
+    return `${lunar}${lunar.getTimeZhi()}时`;
+};
 
 /**
  * 获取阳历日期
  */
 const getSolarDate = () => {
-    const solar = Solar.fromDate(new Date())
-    return `${solar.toString()}日${solar.getHour()}点`
-}
+    const solar = Solar.fromDate(new Date());
+    return `${solar.toString()}日${solar.getHour()}点`;
+};
+
+const startScrollAnimation = <T extends Element | null>(refNode: T) => {
+    const scrollAnimation = <T extends Element | null>(refNode: T) => {
+        const nextNodeRef: { nextNode: Element | null } = {
+            nextNode: null,
+        };
+
+        const scrollOptions: ScrollToOptions = {
+            left: 0,
+            top: refNode?.scrollHeight,
+            behavior: "smooth",
+        };
+
+        if (refNode && refNode.children.length <= 1) {
+            Promise.resolve().then(() => {
+                scrollAnimation(nextNodeRef.nextNode);
+            });
+        } else {
+            refNode?.addEventListener("scrollend", () => {
+                console.log(nextNodeRef.nextNode);
+                if (nextNodeRef.nextNode) {
+                    scrollAnimation(nextNodeRef.nextNode);
+                }
+            });
+            refNode?.scrollTo(scrollOptions);
+        }
+
+        return {
+            next: <T extends Element | null>(refNode: T) => {
+                nextNodeRef.nextNode = refNode;
+                return scrollAnimation(refNode);
+            },
+        };
+    };
+
+    return scrollAnimation(refNode);
+};
 
 /**
  * 计算小六壬结果
  */
 const getDivination = () => {
-    const currentSolar = Solar.fromDate(new Date())
-    const currentLunar = currentSolar.getLunar()
+    const currentSolar = Solar.fromDate(new Date());
+    const currentLunar = currentSolar.getLunar();
 
-    const lunarMonthIndex = currentLunar.getMonth()
-    const lunarDayIndex = currentLunar.getDay()
-    const lunarHourIndex = currentLunar.getTime().getZhiIndex() + 1
+    const lunarMonthIndex = currentLunar.getMonth();
+    const lunarDayIndex = currentLunar.getDay();
+    const lunarHourIndex = currentLunar.getTime().getZhiIndex() + 1;
 
-    let lunarMonthArr = []
+    let lunarMonthArr = [];
     for (let i = 0; i < lunarMonthIndex; i++) {
-        let index = i % hexagramArray.length
-        lunarMonthArr[i] = hexagramArray[index]
+        let index = i % hexagramArray.length;
+        lunarMonthArr[i] = hexagramArray[index];
     }
-    state.lunarMonthArr = lunarMonthArr
+    state.lunarMonthArr = lunarMonthArr;
 
-    let startDayIndex = lunarMonthArr.length > 0 ? hexagramArray.indexOf(lunarMonthArr[lunarMonthArr.length - 1]) : 0
-    let lunarDayArr = []
+    let startDayIndex = lunarMonthArr.length > 0 ? hexagramArray.indexOf(lunarMonthArr[lunarMonthArr.length - 1]) : 0;
+    let lunarDayArr = [];
     for (let i = 0; i < lunarDayIndex; i++) {
-        let index = (startDayIndex + i) % hexagramArray.length
-        lunarDayArr[i] = hexagramArray[index]
+        let index = (startDayIndex + i) % hexagramArray.length;
+        lunarDayArr[i] = hexagramArray[index];
     }
-    state.lunarDayArr = lunarDayArr
+    state.lunarDayArr = lunarDayArr;
 
-    let startHourIndex = state.lunarDayArr.length > 0 ? hexagramArray.indexOf(lunarDayArr[lunarDayArr.length - 1]) : 0
-    let lunarHourArr = []
+    let startHourIndex = state.lunarDayArr.length > 0 ? hexagramArray.indexOf(lunarDayArr[lunarDayArr.length - 1]) : 0;
+    let lunarHourArr = [];
     for (let i = 0; i < lunarHourIndex; i++) {
-        let index = (startHourIndex + i) % hexagramArray.length
-        lunarHourArr[i] = hexagramArray[index]
+        let index = (startHourIndex + i) % hexagramArray.length;
+        lunarHourArr[i] = hexagramArray[index];
     }
-    state.lunarHourArr = lunarHourArr
+    state.lunarHourArr = lunarHourArr;
 
     //日时解析
-    state.fortuneMsg = fortuneArray
-        [hexagramArray.findIndex((item) => {
-        return item.type === lunarDayArr[lunarDayArr.length - 1].type
-    })]
-        [hexagramArray.findIndex((item) => {
-        return item.type === lunarHourArr[lunarHourArr.length - 1].type
-    })].msg
+    state.fortuneMsg =
+        fortuneArray[hexagramArray.findIndex((item) => item.type === lunarDayArr[lunarDayArr.length - 1].type)][
+            hexagramArray.findIndex((item) => item.type === lunarHourArr[lunarHourArr.length - 1].type)
+        ].msg;
 
     nextTick(() => {
-        ref1.value?.scrollTo({ left: 0, top: ref1.value.scrollHeight, behavior: 'smooth' })
-        ref1.value?.addEventListener('scrollend', (_) => {
-            ref2.value?.scrollTo({ left: 0, top: ref2.value.scrollHeight, behavior: 'smooth' })
-            ref2.value?.addEventListener('scrollend', (_) => {
-                ref3.value?.scrollTo({ left: 0, top: ref3.value.scrollHeight, behavior: 'smooth' })
-            })
-        })
-    })
-}
+        startScrollAnimation(ref1.value).next(ref2.value).next(ref3.value);
+    });
+};
 </script>
 
 <style scoped>
-
 .resultDiv {
     display: flex;
 }
@@ -193,5 +220,4 @@ const getDivination = () => {
 .result::-webkit-scrollbar {
     display: none;
 }
-
 </style>
